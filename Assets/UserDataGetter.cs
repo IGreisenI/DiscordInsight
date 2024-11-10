ca7f611367dfcd8dd7b0117e75770f5fe22465b3
stjepanknezevic <kstjepan4@gmail.com>
2024-11-10 19:16:10 +0100

    fix: remove button and go directly from start

--- Assets/UserDataGetter.cs
+++ Assets/UserDataGetter.cs
@@ -7,16 +7,16 @@ public class UserDataGetter : MonoBehaviour
     [SerializeField] private TMP_Text userIdTextUI;
     [SerializeField] private TMP_Text serverInfoTextUI;
 
-    async void GetUserData()
+    async void Start()
     {
         var user = await GetUser();
         userIdTextUI.SetText(user.username);
-        
+
         SubActivityInstanceParticipantsUpdate((data) => {
             Debug.Log("Received a participants update!");
         });
 
-        var channel = await GetChannel(await GetChannelId());
-        serverInfoTextUI.SetText(channel.name);
+        var channel = await GetChannelId();
+        serverInfoTextUI.SetText(channel);
     }
 }
